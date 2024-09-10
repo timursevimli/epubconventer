10638415dc433d89f37b9dddab1511687656f576
Timur Sevimli <svmlitimur@gmail.com>
2024-09-10 16:33:59 +0300

    Add bin

---
+++ bin/convert.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+'use strict';
+
+const EpubConverter = require('../lib/convert.js');
+
+const parseArgs = () =>
+  process.argv.slice(2).reduce((args, arg) => {
+    const [key, value] = arg.slice(2).split('=');
+    if (value) {
+      if (value === 'true') args[key] = true;
+      else if (value === 'false') args[key] = false;
+      else args[key] = value;
+    }
+    return args;
+  }, {});
+
+const { target, output } = parseArgs();
+
+new EpubConverter(target, output).convert();
